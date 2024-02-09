e2ce6ba35782f6f32c6e1a6315c3f8d34e92b00e
A. Lincoln Jarwee <122073281+ajarwee-lin@users.noreply.github.com>
2024-02-08 20:47:54 -0700

    commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const Mongodb = require('./db/connect');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const app = express();
+
+app 
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', require('./routes'));
+
+mongodb.initDB((err, db) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`Connected to DB and listening on ${port}`);
+        });
+    }
+});
